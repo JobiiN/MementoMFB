b1e839153d7b5651bbc13a21b4a9c299cac3215b
Iteoluwakiishi <iteoluwaadejobi@gmail.com>
2026-06-09 12:26:37 +0100

    mail update

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -37,7 +37,7 @@ function Footee() {
                 <div className="mfb-footer__card-body">
                   <p className="mfb-footer__card-label">Email</p>
                   <div className="mfb-footer__card-value">
-                    <a href="mailto:info@mementomfb.com">info@memento.ng</a>
+                    <a href="mailto:info@memento.ng">info@memento.ng</a>
                   </div>
                 </div>
               </div>
